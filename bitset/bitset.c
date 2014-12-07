2cd4439cf3dc3fc0b8fa1a50f4ce886b5594d79d
nirenjan <nirenjan@gmail.com>
2014-12-07 15:52:47 -0800

    Update bitset and add Doxygen support

--- bitset/bitset.c
+++ bitset/bitset.c
@@ -11,129 +11,298 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include "bitset.h"
 
-struct bitset_s {
-    uintptr_t length;
-    unsigned int *map;
+/**********************************************************************
+ * Merge Operators
+ *
+ * These utility functions are used for the corresponding merge
+ * operations by the update and merge wrappers
+ **********************************************************************/
+static unsigned int bitset_merge_and(unsigned int a, unsigned int b)
+{
+    return (a & b);
+}
+
+static unsigned int bitset_merge_andn(unsigned int a, unsigned int b)
+{
+    return (a & ~b);
+}
+
+static unsigned int bitset_merge_or(unsigned int a, unsigned int b)
+{
+    return (a | b);
+}
+
+static unsigned int bitset_merge_xor(unsigned int a, unsigned int b)
+{
+    return (a ^ b);
+}
+
+static unsigned int bitset_merge_not(unsigned int a, unsigned int b)
+{
+    b = b; /* To avoid unused variable warnings */
+    return (~a);
+}
+
+/* This is the merge utility function type */
+typedef unsigned int (*bitset_merge_fn)(unsigned int, unsigned int);
+
+/*
+ * This function is used to find the minimum of two size_t's. It's used
+ * in preference to a macro to avoid potential pitfalls.
+ */
+static inline size_t min(size_t a, size_t b)
+{
+    return ((a > b) ? b : a);
+}
+
+/*
+ * The bitset is stored as a struct with a length (in number of bits),
+ * and a pointer to the actual map. The map is an array of unsigned int's,
+ * rather than a fixed size, thereby allowing the compiler to optimize
+ * for the architecture being built for.
+ */
+struct bitset {
+    size_t length;      /* Number of bits in the bitset */
+    size_t map_length;  /* Number of ints in the map array */
+    unsigned int *map;  /* Pointer to the map array */
 };
 
 #define BITS_PER_INDEX (sizeof(unsigned int) * 8)
 
-bitset_t * bitset_init(size_t length)
+/*
+ * Utility function to compute the map size for a given bitset size
+ */
+static inline size_t bitset_map_size(size_t bitset_size)
 {
-    bitset_t *bitset;
+    return ((bitset_size + BITS_PER_INDEX - 1) / BITS_PER_INDEX);
+}
+
+/*
+ * bitset_malloc
+ *
+ * Allocate a bitset of the given size
+ */
+struct bitset * bitset_malloc(size_t length)
+{
+    struct bitset *set;
     size_t bitset_length;
 
-    bitset = (bitset_t *)malloc(sizeof(bitset_t));
+    set = (struct bitset *)malloc(sizeof(struct bitset));
 
-    if (bitset == NULL) {
+    if (set == NULL) {
         /* Unable to allocate bitset root */
         errno = ENOMEM;
         return (NULL);
     }
 
     /* Allocate as many bytes as needed for the bitset itself */
-    bitset_length = (length + BITS_PER_INDEX - 1) / BITS_PER_INDEX;
-    bitset->length = length;
-    bitset->map = calloc(bitset_length, sizeof(unsigned int));
+    bitset_length = bitset_map_size(length);
+    set->length = length;
+    set->map_length = bitset_length;
+    set->map = malloc(bitset_length * sizeof(unsigned int));
 
-    if (!bitset->map) {
+    if (!set->map) {
         /* Unable to allocate bitset. Free the bitset root and return NULL */
-        free(bitset);
+        free(set);
         errno = ENOMEM;
         return (NULL);
     }
 
-    return (bitset);
+    return (set);
 }
 
-void bitset_free(bitset_t *bitset)
+/*
+ * bitset_dup
+ *
+ * Make a duplicate of a bitset
+ */
+struct bitset * bitset_dup(struct bitset *set)
 {
-    if (bitset) {
-        if (bitset->map) {
-            free(bitset->map);
-            bitset->map = NULL;
-        }
-        bitset->length = 0;
-        free(bitset);
+    struct bitset * new_bitset;
+
+    if (!set || !set->map) {
+        errno = EINVAL;
+        return NULL;
     }
+
+    new_bitset = bitset_malloc(set->length);
+    if (!new_bitset) {
+        /* Errno has already been set to ENOMEM by bitset_malloc */
+        return NULL;
+    }
+
+    /* Copy the contents of the original bitset to the new bitset */
+    memcpy(new_bitset->map, set->map,
+            set->map_length * sizeof(unsigned int));
+
+    return new_bitset;
 }
 
-int bitset_set(bitset_t *bitset, size_t bit)
+/*
+ * bitset_resize
+ *
+ * Resize a given bitset to the new size
+ */
+int bitset_resize(struct bitset *set, size_t length)
 {
-    size_t index = bit / BITS_PER_INDEX;
-    size_t offset = bit % BITS_PER_INDEX;
+    size_t new_map_length;
+    unsigned int *new_map;
+    unsigned int *old_map;
 
-    if (bitset) {
-        if (bitset->length >= bit) {
-            bitset->map[index] |= (1 << offset);
-        } else {
-            errno = ERANGE;
-            return 1;
-        }
-    } else {
+    if (!set || !set->map) {
         errno = EINVAL;
         return 1;
     }
 
-    return 0;
-}
+    new_map_length = bitset_map_size(length);
+    if (new_map_length == set->map_length) {
+        /* 
+         * No need to resize the array itself, just go ahead
+         * and update the length
+         */
+        set->length = length;
+        return 0;
+    }
 
-int bitset_clr(bitset_t *bitset, size_t bit)
-{
-    size_t index = bit / BITS_PER_INDEX;
-    size_t offset = bit % BITS_PER_INDEX;
-
-    if (bitset) {
-        if (bitset->length >= bit) {
-            bitset->map[index] &= ~(1 << offset);
-        } else {
-            errno = ERANGE;
-            return 1;
-        }
-    } else {
-        errno = EINVAL;
+    old_map = set->map;
+    new_map = realloc(old_map, new_map_length * sizeof(unsigned int));
+    if (!new_map) {
+        /* Failed to allocate memory. Retain the existing map */
+        errno = ENOMEM;
         return 1;
     }
 
+    set->length = length;
+    set->map_length = new_map_length;
+    set->map = new_map;
+
     return 0;
 }
 
-int bitset_tgl(bitset_t *bitset, size_t bit)
+void bitset_free(struct bitset *set)
+{
+    if (set) {
+        if (set->map) {
+            free(set->map);
+            set->map = NULL;
+        }
+        set->length = 0;
+        set->map_length = 0;
+        free(set);
+    }
+}
+
+static int bitset_update(struct bitset *set, size_t bit, bitset_merge_fn fn)
 {
     size_t index = bit / BITS_PER_INDEX;
     size_t offset = bit % BITS_PER_INDEX;
+    int retval = 0;
 
-    if (bitset) {
-        if (bitset->length >= bit) {
-            bitset->map[index] ^= (1 << offset);
+    if (set && set->map) {
+        if (set->length >= bit) {
+            set->map[index] = fn(set->map[index], (1 << offset));
         } else {
             errno = ERANGE;
-            return 1;
+            retval = 1;
         }
     } else {
         errno = EINVAL;
-        return 1;
+        retval = 1;
     }
 
-    return 0;
+    return retval;
 }
 
-#define MIN(a,b) ((a) > (b) ? (b) : (a))
+int bitset_set(struct bitset *set, size_t bit)
+{
+    return bitset_update(set, bit, bitset_merge_or);
+}
 
-int bitset_and(bitset_t *dst, bitset_t *src)
+int bitset_reset(struct bitset *set, size_t bit)
+{
+    return bitset_update(set, bit, bitset_merge_andn);
+}
+
+int bitset_toggle(struct bitset *set, size_t bit)
+{
+    return bitset_update(set, bit, bitset_merge_xor);
+}
+
+static int bitset_merge(struct bitset *dst, struct bitset *src, bitset_merge_fn fn)
 {
     size_t comp_length;
+    size_t i;
 
-    if (!dst || !src) {
+    if (!dst || !src || !dst->map || !src->map) {
         errno = EINVAL;
         return 1;
     }
 
-    comp_length = MIN(dst->length, src->length);
-    comp_length = (comp_length + BITS_PER_INDEX - 1) / BITS_PER_INDEX;
+    comp_length = min(dst->map_length, src->map_length);
 
+    for (i = 0; i < comp_length; i++) {
+        /* Merge the two bitsets */
+        dst->map[i] = fn(dst->map[i], src->map[i]);
+    }
+
+    return 0;
+}
+
+int bitset_and(struct bitset *dst, struct bitset *src)
+{
+    return bitset_merge(dst, src, bitset_merge_and);
+}
+
+int bitset_or(struct bitset *dst, struct bitset *src)
+{
+    return bitset_merge(dst, src, bitset_merge_or);
+}
+
+int bitset_xor(struct bitset *dst, struct bitset *src)
+{
+    return bitset_merge(dst, src, bitset_merge_xor);
+}
+
+int bitset_andn(struct bitset *dst, struct bitset *src)
+{
+    return bitset_merge(dst, src, bitset_merge_andn);
+}
+
+int bitset_not(struct bitset *dst)
+{
+    /*
+     * The src operand is dummy in this case, it's just required by the
+     * bitset_merge function, but ignored by the bitset_merge_not function
+     */
+    return bitset_merge(dst, dst, bitset_merge_not);
+}
+
+int bitset_clear(struct bitset *set)
+{
+    if (!set || !set->map) {
+        errno = EINVAL;
+        return 1;
+    }
+
+    memset(set->map, 0, set->map_length * sizeof(unsigned int));
+
+    return 0;
+}
+
+int bitset_test(struct bitset *set, size_t bit, unsigned int *value)
+{
+    size_t index = bit / BITS_PER_INDEX;
+    size_t offset = bit % BITS_PER_INDEX;
+
+    if (!set || !set->map || !value) {
+        errno = EINVAL;
+        return 1;
+    }
+
+    *value = !!(set->map[index] & (1 << offset));
     return 0;
 }
--- bitset/bitset.h
+++ bitset/bitset.h
@@ -27,56 +27,119 @@ __CDECL_BEGIN
  *
  * All the bitset APIs will be passed a pointer to this data type.
  */
-typedef struct bitset_s bitset_t;
+struct bitset;
+
+/** @name Memory management
+ * @{
+ */
 
 /** @brief Create a new bitset
  *
  * Create a new bitset of the given length.
  *
- * @param   length  The desired length of the bitset. This cannot be changed
- *                  during runtime.
+ * @param   length  The desired length of the bitset.
  *
- * @returns Pointer to the allocated bitset with all bits set to zero. If it
- *          cannot allocate the necessary memory, it returns NULL and sets
- *          errno accordingly.
+ * @returns Pointer to the allocated bitset. If it cannot allocate the
+ *          necessary memory, it returns NULL and sets errno accordingly. No
+ *          guarantee is made on the state of the bits.
  */
-bitset_t * bitset_init(size_t length);
+struct bitset * bitset_malloc(size_t length);
+
+/** @brief Resize an initialized bitset
+ *
+ * Resizes an allocated bitset to the new size. The bit state is copied to
+ * the resized bitset, but truncated if the new bitset is smaller. If the
+ * new bitset is larger, the state of the new bits is undefined.
+ *
+ * @param   set     Pointer to the allocated bitset
+ * @param   length  The desired length of the bitset.
+ *
+ * @returns 0 on success, 1 on failure and sets errno accordingly.
+ */
+int bitset_resize(struct bitset *set, size_t length);
 
 /** @brief Free a previously allocated bitset
  *
- * @param   bitset  Pointer to an allocated bitset. The API will reset the
+ * @param   set     Pointer to an allocated bitset. The API will reset the
  *                  data structures and free the associated memory.
  *
  * @returns nothing
  */
-void bitset_free(bitset_t *bitset);
+void bitset_free(struct bitset *set);
+
+/** @brief Create a copy of a bitset.
+ *
+ * This can be used to make a copy of a bitset. However, if there is
+ * insufficient memory to make the copy, then it will fail.
+ *
+ * @param   set     Pointer to the bitset to duplicate
+ *
+ * @returns Pointer to the duplicated bitset. If it cannot allocate the
+ *          necessary memory, it returns NULL and sets errno accordingly.
+ */
+struct bitset * bitset_dup(struct bitset *set);
+
+/** @} */
+
+/** @name Bitset Update Functions
+ * @{
+ */
 
 /** @brief Set a bit in the bitset
  *
- * @param   bitset  Pointer to an allocated bitset
+ * @param   set     Pointer to an allocated bitset
  * @param   bit     Index of the bit to set
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly
  */
-int bitset_set(bitset_t *bitset, size_t bit);
+int bitset_set(struct bitset *set, size_t bit);
 
-/** @brief Clear a bit in the bitset
+/** @brief Reset a bit in the bitset
  *
- * @param   bitset  Pointer to an allocated bitset
+ * @param   set     Pointer to an allocated bitset
  * @param   bit     Index of the bit to clear
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly
  */
-int bitset_clr(bitset_t *bitset, size_t bit);
+int bitset_reset(struct bitset *set, size_t bit);
 
 /** @brief Toggle a bit in the bitset
  *
- * @param   bitset  Pointer to an allocated bitset
+ * @param   set     Pointer to an allocated bitset
  * @param   bit     Index of the bit to toggle
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly
  */
-int bitset_tgl(bitset_t *bitset, size_t bit);
+int bitset_toggle(struct bitset *set, size_t bit);
+
+/** @brief Take the complement of a bitset
+ *
+ * Take the complement of the given bitset and store it in the same location.
+ * The complement inverts all bits, that is, all 0 bits become 1 bits and
+ * vice versa.
+ *
+ * @param   dst     Pointer to the bitset
+ *
+ * @returns 0 on success, 1 on failure and sets errno accordingly.
+ */
+int bitset_not(struct bitset *dst);
+
+/** @brief Clear all the bits in a bitset
+ *
+ * Clear all bits in the bitset. This is typically used after @ref
+ * bitset_malloc to get to a known state.
+ *
+ * @param   set     Pointer to the bitset
+ *
+ * @returns 0 on success, 1 on failure and sets errno accordingly.
+ */
+int bitset_clear(struct bitset *set);
+
+/** @} */
+
+/** @name Bitset Merge Functions
+ * @{
+ */
 
 /** @brief Take the intersection of two bitsets
  *
@@ -91,7 +154,7 @@ int bitset_tgl(bitset_t *bitset, size_t bit);
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly.
  */
-int bitset_and(bitset_t *dst, bitset_t *src);
+int bitset_and(struct bitset *dst, struct bitset *src);
 
 /** @brief Take the union of two bitsets
  *
@@ -106,7 +169,7 @@ int bitset_and(bitset_t *dst, bitset_t *src);
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly.
  */
-int bitset_or(bitset_t *dst, bitset_t *src);
+int bitset_or(struct bitset *dst, struct bitset *src);
 
 /** @brief Take the symmetric difference of two bitsets
  *
@@ -122,20 +185,29 @@ int bitset_or(bitset_t *dst, bitset_t *src);
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly.
  */
-int bitset_xor(bitset_t *dst, bitset_t *src);
+int bitset_xor(struct bitset *dst, struct bitset *src);
 
-/** @brief Take the complement of a bitset
+/** @name Bitset Access Functions
+ * @{
+ */
+
+/** @brief Test the value of a bit
  *
- * Take the complement of the given bitset and store it in the same location.
- * The complement inverts all bits, that is, all 0 bits become 1 bits and
- * vice versa.
+ * Return the status of the bit in the given pointer.
  *
- * @param   dst     Pointer to the bitset
+ * @param   set     Pointer to the bitset
+ * @param   bit     Index of the bit to test
+ * @param   value   Pointer to the return value. If this is NULL, then the
+ *                  call will fail. The value is either 0 or 1 corresponding
+ *                  to the actual bit value.
  *
  * @returns 0 on success, 1 on failure and sets errno accordingly.
  */
-int bitset_not(bitset_t *dst);
+int bitset_test(struct bitset *set, size_t bit, unsigned int *value);
 
+/** @} */
+
+/** @} */
 
 /** @} */
 
